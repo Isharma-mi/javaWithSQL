11014dab11bead2f75249dc7c083ccca805100c1
Ishaan Sharma <107057912+Isharma-mi@users.noreply.github.com>
2024-04-29 18:45:13 -0400

    Created files/Setup project to work with Github Desktop

---
+++ src/com/example/JavaWithSQL/logic/Main.java
@@ -0,0 +1,8 @@
+package com.example.JavaWithSQL.logic;
+
+public class Main {
+	// TODO: Implement table creation from Java into SQL
+	// TODO: Implement adding records from Java into SQL
+	// TODO: Implement removing records from Java into SQL
+	// TODO: Implement viewing of tables from SQL into Java
+}
